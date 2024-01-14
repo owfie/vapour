a029d88ebb4969376e10703b03643700c9c3c07d
Alfie Edgeworth <0xalfie@gmail.com>
2024-01-15 01:29:58 +1030

    add basic client-side fetching and debounce for index

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,12 @@ import { Button, Pane, TextField } from '@/components'
 import styles from './page.module.scss'
 import { motion } from 'framer-motion'
 import { useEffect, useState } from 'react'
+import { VapourPlayer } from '@/utils/SteamMethods'
 
 export default function Home() {
   const [value, setValue] = useState('')
   const [isFetching, setIsFetching] = useState(false)
+  const [vapourUser, setVapourUser] = useState<VapourPlayer | null>(null)
 
   const handleInputChange = (s: string) => {
     setValue(s)
@@ -22,12 +24,17 @@ export default function Home() {
         try {
           console.log('Fetching data...')
           setIsFetching(true)
+          const response = await fetch('/api/players?id=' + value)
+          const data = await response.json()
+          setVapourUser(data)
         } catch (error) {
           console.error('Error fetching data:', error)
+          setVapourUser(null)
         } finally {
+          setIsFetching(false)
           timeoutId = undefined
         }
-      }, 2000)
+      }, 1000)
 
     return () => {
       clearTimeout(timeoutId)
@@ -58,6 +65,7 @@ export default function Home() {
           Test button
         </Button>
         {isFetching && 'fetching'}
+        {vapourUser ? vapourUser.username : 'no user'}
       </Pane>
     </main>
   )
