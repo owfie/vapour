6bf82274e608d377dafe5b2b4216ab80d62451f7
Alfie Edgeworth <0xalfie@gmail.com>
2024-01-15 00:00:47 +1030

    update spacings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,23 @@
+'use client'
+
 import { Button, Pane } from '@/components'
 import styles from './page.module.scss'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-    <Button>
-      Test button
-    </Button>
-    <Pane>
-      Example content
-    </Pane>
+      <h1>Vapour.gg</h1>
+      <h3>Unleash your Steam library.</h3>
+      <Button
+        onClick={() => {
+          console.log('test')
+        }}
+      >
+        Test button
+      </Button>
+      <Pane>
+        Example content
+      </Pane>
     </main>
   )
 }
